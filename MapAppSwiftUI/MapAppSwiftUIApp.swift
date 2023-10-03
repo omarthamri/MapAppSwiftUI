5ff186fa3500464f1dafeed8d592908040495858
Omar Thamri <omarthamri4@gmail.com>
2023-10-03 15:21:42 +0100

    Initial Commit

---
+++ MapAppSwiftUI/MapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftUIApp.swift
+//  MapAppSwiftUI
+//
+//  Created by omar thamri on 3/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
