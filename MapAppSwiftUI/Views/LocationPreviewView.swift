6ea67c5b84949258083c9d1c5d372fd3cb97a195
Omar Thamri <omarthamri4@gmail.com>
2023-10-09 03:31:20 +0100

    Location Preview cards with asymmetric Transitions

---
+++ MapAppSwiftUI/Views/LocationPreviewView.swift
@@ -0,0 +1,90 @@
+//
+//  LocationPreviewView.swift
+//  MapAppSwiftUI
+//
+//  Created by omar thamri on 8/10/2023.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    let location: Location
+    var body: some View {
+        HStack(alignment: .bottom,spacing: 0) {
+            VStack(alignment: .leading,spacing: 16) {
+            imageSection
+            titleSection
+        }
+            VStack(spacing: 8) {
+                learnButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+        )
+    }
+}
+
+struct LocationPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+        LocationPreviewView(location: LocationsDataService.locations.first!)
+        }
+    }
+}
+
+extension LocationPreviewView {
+    
+    private var imageSection: some View {
+        ZStack {
+            if let imageName = location.imageNames.first {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading,spacing: 4) {
+        Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+        Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity,alignment: .leading)
+    }
+    
+    private var learnButton: some View {
+        Button {
+            
+        } label: {
+            Text("Learn more")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button {
+            
+        } label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
+    }
+    
+}
