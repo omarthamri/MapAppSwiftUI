9747734a4cfda1bd7a7c546c2c15ff6de2051db3
Omar Thamri <omarthamri4@gmail.com>
2023-10-13 18:26:33 +0100

    Custom Map Annotation Pins for SwiftUI MapKit Map

--- MapAppSwiftUI/Views/LocationsView.swift
+++ MapAppSwiftUI/Views/LocationsView.swift
@@ -14,22 +14,13 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
             VStack(spacing: 0) {
                 header
                 .padding()
                 Spacer()
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                        LocationPreviewView(location: location)
-                            .shadow(color: .black.opacity(0.3), radius: 20)
-                            .padding()
-                            .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
-                        }
-                    }
-                }
+                locationPreviewStack
             }
         }
     }
@@ -68,6 +59,32 @@ extension LocationsView {
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion, annotationItems: vm.locations, annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                LocationPreviewView(location: location)
+                    .shadow(color: .black.opacity(0.3), radius: 20)
+                    .padding()
+                    .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                }
+            }
+        }
     }
     
 }
